369b1771dd7421a7c2098b1b18b7b21b5f3a683c
David Kirby <david@kinetic.com>
2022-09-28 09:38:29 -0500

    adding custom post types

--- footer.php
+++ footer.php
@@ -1,5 +1,16 @@
 <div class="footer">
 
+        <?php 
+        
+        wp_nav_menu(
+            array(
+                'theme_location' => 'footer-menu',
+                'container_class' => 'bottom-nav'
+            )
+            );
+        
+        ?>
+
             <p>author: david kirby <?php echo date('Y'); ?></p>
 
         </div>
--- functions.php
+++ functions.php
@@ -39,6 +39,71 @@ $defaults = array(
 add_theme_support( 'custom-background', $defaults );
 
 
+function story_post() {
+
+    $args = array(
+
+        'labels' => array(
+
+            'name' => 'Story',
+            'singular_name' => 'Story',
+
+        ),
+
+        'public' => true,
+        'has_archive' => true,
+        'supports' => array('title', 'editor', 'thumbnail'),
+    );
+
+    register_post_type('story', $args);
+
+}
+
+add_action('init', 'story_post');
+
+function character_post() {
+
+    $args = array(
+
+        'labels' => array(
+
+            'name' => 'Characters',
+            'singular_name' => 'Character',
+
+        ),
+
+        'public' => true,
+        'has_archive' => true,
+        'supports' => array('title', 'editor', 'thumbnail'),
+    );
+
+    register_post_type('characters', $args);
+
+}
+
+add_action('init', 'character_post');
+
+function gameplay_post() {
+
+    $args = array(
+
+        'labels' => array(
+
+            'name' => 'Gameplay',
+            'singular_name' => 'Gameplay',
+
+        ),
+
+        'public' => true,
+        'has_archive' => true,
+        'supports' => array('title', 'editor', 'thumbnail'),
+    );
+
+    register_post_type('gamepley', $args);
+
+}
+
+add_action('init', 'gameplay_post');
 
 
 ?>
